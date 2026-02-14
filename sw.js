8c0c2fb78f06aba3eb4c83506a5155284fc5f5e4
Sampath <write2sampath@gmail.com>
2026-02-14 14:44:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Bump CACHE_VERSION when you deploy new code so the old cache is replaced.
  */
 
-const CACHE_VERSION = 'upi-decoder-v1';
+const CACHE_VERSION = 'upi-decoder-v2';
 
 /**
  * All local assets that make up the app shell.
